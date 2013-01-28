39e3d130b47682d03d223c5a9db7b44431b3ff50
Daniel Robbins <drobbins@funtoo.org>
2013-01-28 19:59:50 +0000

    install everything to an image/ directory rather than /opt/zenoss4 on the root filesystem

--- build_zenoss.sh
+++ build_zenoss.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+die() {
+	echo $*
+	exit 1
+}
+
+try() {
+	"$@"
+	[ $? -ne 0 ] && echo Failure: $* && exit 1
+}
+
 ORIG_DIR=`pwd`
 BUILDDIR=$ORIG_DIR/Build
 rm -rf $BUILDDIR
@@ -7,8 +18,6 @@ rm -rf $BUILDDIR
 ZENHOME=/opt/zenoss4
 PYTHON_VERSION=2.7
 PYTHON=python${PYTHON_VERSION}
-VIRTUALENV=$ZENHOME/venv
-VIRTUALENV_PROG=virtualenv-$PYTHON_VERSION
 
 # this will vary based on tarball used:
 
@@ -23,39 +32,37 @@ fi
 A=$1
 # expect source archive to have a directory inside with the same name, minus .tar.xz:
 SRCDIR=$BUILDDIR/${1%%.tar.*}
+DESTDIR=$ORIG_DIR/image
 REQUIREMENTS=$2
-
 INSTDIR=$SRCDIR/inst
 export INSTDIR
+
+# These are currently paths inside /opt, need to change so we write to DESTDIR rather than these paths
 export ZENHOME
-export VIRTUALENV
+export VIRTUAL_ENV=$ZENHOME/venv
+# we will tweak virtualenv so we can temporarily run it inside its destination build directory, without hard-coding 
+# the temp build directory path. When zenoss is installed, this variable will be unset and things will operate
+# correctly in their final location:
+export VIRTUAL_ENV_PREFIX=$DESTDIR
 
-die() {
-	echo $*
-	exit 1
-}
-
-try() {
-	"$@"
-	[ $? -ne 0 ] && echo Failure: $* && exit 1
-}
-
-mkdir -p $VIRTUALENV || die "Couldn't create $VIRTUALENV"
 install -d $BUILDDIR || die
 
+mkdir -p $DESTDIR/$VIRTUAL_ENV || die "Couldn't create $VIRTUAL_ENV"
 # Create a virtual environment if it doesnt already exist
-if [ ! -e $VIRTUALENV/bin/activate ]
+if [ ! -e $DESTDIR/$VIRTUAL_ENV/bin/activate ]
 then
-    try mkdir -p $VIRTUALENV
-    $VIRTUALENV_PROG $VIRTUALENV || die "virtualenv fail"
+    try mkdir -p $DESTDIR/$VIRTUAL_ENV
+    virtualenv-$PYTHON_VERSION $DESTDIR/$VIRTUAL_ENV || die "virtualenv fail"
 fi
 
-#Update the environment
-#Change the paths and add a ZENHOME env variable
-sed -i -e 's|PATH="$VIRTUAL_ENV/bin:$PATH"|PATH="$VIRTUAL_ENV/../bin:$VIRTUAL_ENV/bin:$PATH"\nZENHOME='$ZENHOME'\nexport ZENHOME|g' $VIRTUALENV/bin/activate || die "path fix fail"
+#Change the paths and add a ZENHOME env variable:
+# Ensure VIRTUAL_ENV is set to something like /opt/zenoss4/venv (it won't be by default)
+# Ensure VIRTUAL_ENV_PREFIX can be used to move venv inside an image directory.
+
+sed -i -e "s|^VIRTUAL_ENV=.*|VIRTUAL_ENV=$VIRTUAL_ENV|" -e 's|PATH="$VIRTUAL_ENV/bin:$PATH"|PATH="$VIRTUAL_ENV_PREFIX$VIRTUAL_ENV/../bin:$VIRTUAL_ENV_PREFIX$VIRTUAL_ENV/bin:$PATH"\nZENHOME='$ZENHOME'\nexport ZENHOME|g' $DESTDIR/$VIRTUAL_ENV/bin/activate || die "path fix fail"
 
 #Activate the virtual env
-source $VIRTUALENV/bin/activate || die "couldn't activate virtualenv"
+source $DESTDIR/$VIRTUAL_ENV/bin/activate || die "couldn't activate virtualenv"
 
 tar xvf $A -C $BUILDDIR || die "source tar extract fail"
 
@@ -87,24 +94,24 @@ done
 pip install -r $BUILDDIR/requirements.txt.autodetect || die "pip failure"
 
 # Reactivate the virtual environment to update the PATH
-source $VIRTUALENV/bin/activate || die "activate fail"
+source $DESTDIR/$VIRTUAL_ENV/bin/activate || die "activate fail"
 
 # install some directory trees as-is from the source archive:
-cp -a $SRCDIR/bin $ZENHOME/bin || die "bin install fail"
-cp -a $SRCDIR/Products $ZENHOME/Products || die "products install fail"
-cp -a $SRCDIR/inst/fs $ZENHOME/extras || die "extras install fail"
+cp -a $SRCDIR/bin $DESTDIR/$ZENHOME/bin || die "bin install fail"
+cp -a $SRCDIR/Products $DESTDIR/$ZENHOME/Products || die "products install fail"
+cp -a $SRCDIR/inst/fs $DESTDIR/$ZENHOME/extras || die "extras install fail"
 # Create some required directories:
-mkdir -p $ZENHOME/{backups,export,build,etc} || die "standard dir create fail"
+mkdir -p $DESTDIR/$ZENHOME/{backups,export,build,etc} || die "standard dir create fail"
 # Copy the license
 cd $INSTDIR/externallibs || die "cd fail"
 for i in $(ls Licenses.*)
 do
-    cp $i $ZENHOME || die "license $i fail"
+    cp $i $DESTDIR/$ZENHOME || die "license $i fail"
 done
-cp $INSTDIR/License.zenoss $ZENHOME || die "license fail"
+cp $INSTDIR/License.zenoss $DESTDIR/$ZENHOME || die "license fail"
 
 #Setup the sitecustomize file
-SITECUSTOMIZE=$VIRTUALENV/lib/$PYTHON/sitecustomize.py
+SITECUSTOMIZE=$DESTDIR/$VIRTUAL_ENV/lib/$PYTHON/sitecustomize.py
 if [ -f $SITECUSTOMIZE ]
 then
     rm $SITECUSTOMIZE
@@ -122,35 +129,36 @@ EOF
 # Copy in conf files.
 
 # zenoss.conf
-sed -e "s;<<INSTANCE_HOME>>;$ZENHOME;g" $INSTDIR/conf/zenoss.conf.in > $ZENHOME/etc/zenoss.conf || die "zenoss.conf install fail"
+sed -e "s;<<INSTANCE_HOME>>;$ZENHOME;g" $INSTDIR/conf/zenoss.conf.in > $DESTDIR/$ZENHOME/etc/zenoss.conf || die "zenoss.conf install fail"
 
 # global.conf
-if [ ! -f $ZENHOME/etc/global.conf ]
+if [ ! -f $DESTDIR/$ZENHOME/etc/global.conf ]
 then
-    cp $INSTDIR/conf/global.conf $ZENHOME/etc/ || die "global.conf copy fail"
+    cp $INSTDIR/conf/global.conf $DESTDIR/$ZENHOME/etc/ || die "global.conf copy fail"
 fi
 
 cd $INSTDIR/conf || die "conf cd fail"
 for conf in *
 do  
-    if [ ! -f $ZENHOME/etc/$conf.example ]
+    if [ ! -f $DESTDIR/$ZENHOME/etc/$conf ]
     then
-        cp $conf $ZENHOME/etc/$conf.example
-        sed -i -e 's/ZENUSERNAME/$(ZOPEUSER)/' -e 's/ZENPASSWORD/$(ZOPEPASSWORD)/' $ZENHOME/etc/$conf.example || die "$conf fail"
+        cp $conf $DESTDIR/$ZENHOME/etc/$conf
+        sed -i -e 's/ZENUSERNAME/$(ZOPEUSER)/' -e 's/ZENPASSWORD/$(ZOPEPASSWORD)/' $DESTDIR/$ZENHOME/etc/$conf || die "$conf fail"
     fi
 done
 
 # Copy in the skel files?
 # Compile protocol buffers
 # This tool is only required if we are going to compile them on our own.
-if [ ! -e $ZENHOME/bin/protoc ]
+if [ ! -e $DESTDIR/$ZENHOME/bin/protoc ]
 then
     cd $BUILDDIR
     tar xvf protobuf*tar* || die "protobuf extract fail"
     cd protobuf*
     ./configure --prefix=$ZENHOME --enable-shared=yes --enable-static=no || die "protobuf configure fail"
     make ${MAKEOPTS} || die "protobuf build fail"
-    make install || die "protobuf install fail"
+    make DESTDIR=$DESTDIR install || die "protobuf install fail"
+# TODO: CHECK SETUP.PY INSTALL::::
     cd python/
     python setup.py install || die "protobuf python install fail"
 fi
@@ -159,41 +167,43 @@ fi
 #$ZENHOME is provided as part of the virtualenv environment and so thats how this knows where to go.
 cd $INSTDIR/zensocket
 make ${MAKEOPTS} || die "zensocket build fail"
-make install || "zensocket install fail"
+
+# TODO: NEED TO CHECK THIS DESTDIR:
+make DESTDIR=$DESTDIR install || "zensocket install fail"
 
 #Make pyraw
 # We need to patch this to make it venv aware.
 if [ ! -f $INSTDIR/icmpecho/venv.patch ]
 then
     cp $ORIG_DIR/patches/venv.patch $INSTDIR/icmpecho/venv.patch || die "venv patch copy fail"
-    ( cd $INSTDIR/icmpecho; patch -p0 < venv.patch ) || die "venv patch fail"
+    ( cd $INSTDIR/icmpecho; patch -p1 < venv.patch ) || die "venv patch fail"
 fi
 
 cd $INSTDIR/icmpecho
-make ${MAKEOPTS} || die "icmpecho fail"
+make VIRTUAL_ENV=/venv/ DESTDIR=$DESTDIR ${MAKEOPTS} || die "icmpecho fail"
 
 # fix the python path by finding the virtualenv python in the path right now, hard-code it into zenfunctions:
-sed -i -e 's|PYTHON=$ZENHOME/bin/python|PYTHON='`which $PYTHON`'|g' $ZENHOME/bin/zenfunctions || die "python path fix fail"
+sed -i -e 's|PYTHON=$ZENHOME/bin/python|PYTHON='`which $PYTHON`'|g' $DESTDIR/$ZENHOME/bin/zenfunctions || die "python path fix fail"
 # TODO: add zendmd fix.
 
 # build nmap, because we need it in the code and we set it setuid
-if [ ! -e $ZENHOME/bin/nmap ]
+if [ ! -e $DESTDIR/$ZENHOME/bin/nmap ]
 then
     cd $BUILDDIR
     tar -xvf $INSTDIR/externallibs/nmap-* || die "nmap extract fail"
     cd $(ls -lda nmap*|grep ^drwx|awk '{print $9}') || die "nmap cd fail"
     ./configure --prefix=$ZENHOME --without-zenmap --without-ndiff || die "nmap configure fail"
     make ${MAKEOPTS} || die "nmap build fail"
-    make install || die "nmap install fail"
+    make DESTDIR=$DESTDIR install || die "nmap install fail"
 fi
 
-install -d $ZENHOME/share/mibs/site || die "mibs/site mkdir fail"
-if [ ! -e $ZENHOME/share/mibs/site/ZENOSS-MIB.txt ]
+install -d $DESTDIR/$ZENHOME/share/mibs/site || die "mibs/site mkdir fail"
+if [ ! -e $DESTDIR/$ZENHOME/share/mibs/site/ZENOSS-MIB.txt ]
 then
-    cp $INSTDIR/mibs/* $ZENHOME/share/mibs/site || die "mibs install fail"
+    cp $INSTDIR/mibs/* $DESTDIR/$ZENHOME/share/mibs/site || die "mibs install fail"
 fi
 # Install libsmi
-if [ ! -e $ZENHOME/bin/smidump ]
+if [ ! -e $DESTDIR/$ZENHOME/bin/smidump ]
 then
     rm -rf $BUILDDIR/libsmi*
     cp $INSTDIR/externallibs/libsmi-* $BUILDDIR
@@ -202,7 +212,7 @@ then
     cd $(ls -lda libsmi*|grep ^drwx|awk '{print $9}') || die "libsmi cd fail"
     ./configure --prefix=$ZENHOME || die "libsmi configure fail"
     make ${MAKEOPTS} || die "libsmi build fail"
-    make install || die "libsmi install fail"
+    make DESTDIR=$DESTDIR install || die "libsmi install fail"
 fi
 
 ##### mvn/oracle dependancies below ####
@@ -211,35 +221,35 @@ cd $SRCDIR/java/
 mvn clean install || die "core java build fail"
 # Compile the protocols
 cd $SRCDIR/protocols/
-PATH=$ZENHOME/bin/:${PATH} LD_LIBRARY_PATH=$ZENHOME/lib mvn -f java/pom.xml clean install || die "java protocol build fail"
-PATH=$ZENHOME/bin/:${PATH} LD_LIBRARY_PATH=$ZENHOME/lib make -C python clean build || die "python protocol build fail"
+PATH=$DESTDIR/$ZENHOME/bin/:${PATH} LD_LIBRARY_PATH=$DESTDIR/$ZENHOME/lib mvn -f java/pom.xml clean install || die "java protocol build fail"
+PATH=$DESTDIR/$ZENHOME/bin/:${PATH} LD_LIBRARY_PATH=$DESTDIR/$ZENHOME/lib make -C python clean build || die "python protocol build fail"
 cd python/
 python setup.py install | die "python protocol install fail"
 
 #compile zep
 try cd $SRCDIR/zep
-PATH=$ZENHOME/bin/:${PATH} LD_LIBRARY_PATH=$ZENHOME/lib mvn clean install || die "zep build fail"
+PATH=$DESTDIR/$ZENHOME/bin/:${PATH} LD_LIBRARY_PATH=$DESTDIR/$ZENHOME/lib mvn clean install || die "zep build fail"
 
 #Install zep
 ZEPDIST=$(ls -1 $SRCDIR/zep/dist/target/zep-dist-*.tar.gz)
-(cd $ZENHOME;tar zxvhf $ZEPDIST) || die "zepdist extract fail"
+(cd $DESTDIR/$ZENHOME;tar zxvhf $ZEPDIST) || die "zepdist extract fail"
 
 # Compile the javascript , requires oracle java 1.6+
 cp $INSTDIR/externallibs/JSBuilder2.zip $BUILDDIR/
 (cd $BUILDDIR/;unzip -o JSBuilder2.zip) || die "JSBuilder2 unzip fail"
-JSBUILDER=$BUILDDIR/JSBuilder2.jar ZENHOME=$ZENHOME $INSTDIR/buildjs.sh || die "JS compile failed"
+JSBUILDER=$BUILDDIR/JSBuilder2.jar DESTDIR=$DESTDIR ZENHOME=$ZENHOME $INSTDIR/buildjs.sh || die "JS compile failed"
 
 echo "Setting permissions..."
-chown -R zenoss:zenoss $ZENHOME || die "Couldn't set permissions"
+chown -R zenoss:zenoss $DESTDIR/$ZENHOME || die "Couldn't set permissions"
 
 # Maven does some nasty things with permissions. This is to fix that:
-find $ZENHOME -type d -exec chmod 0775 {} \; || die "dir perm fix"
-find $ZENHOME/webapps -type f -exec chmod 0664 {} \; || die "maven file fix"
-chmod 0775 $ZENHOME/bin/* || die "bin perm normalization"
-chown root:zenoss $ZENHOME/bin/nmap $ZENHOME/bin/pyraw $ZENHOME/bin/zensocket || die "nmap/praw owner fail"
-chmod 04750 $ZENHOME/bin/nmap $ZENHOME/bin/pyraw $ZENHOME/bin/zensocket || die "nmap/praw suid root fail"
+find $DESTDIR/$ZENHOME -type d -exec chmod 0775 {} \; || die "dir perm fix"
+find $DESTDIR/$ZENHOME/webapps -type f -exec chmod 0664 {} \; || die "maven file fix"
+chmod 0775 $DESTDIR/$ZENHOME/bin/* || die "bin perm normalization"
+chown root:zenoss $DESTDIR/$ZENHOME/bin/nmap $DESTDIR/$ZENHOME/bin/pyraw $DESTDIR/$ZENHOME/bin/zensocket || die "nmap/praw owner fail"
+chmod 04750 $DESTDIR/$ZENHOME/bin/nmap $DESTDIR/$ZENHOME/bin/pyraw $DESTDIR/$ZENHOME/bin/zensocket || die "nmap/praw suid root fail"
 # security fix - don't expose passwords to people who shouldn't see it:
-chmod -R o-rwx $ZENHOME/etc || die "security fix fail"
+chmod -R o-rwx $DESTDIR/$ZENHOME/etc || die "security fix fail"
 
 echo "Done!"
 
